cfd98b94494c7df80ecce28bb7045777156ac6b9
Mosè Giordano <mose@gnu.org>
2025-07-19 19:47:44 +0100

    Add ability to plot data while reading it

--- example/arduino_dcf77.jl
+++ example/arduino_dcf77.jl
@@ -1,6 +1,8 @@
 using LibSerialPort
 
-function read_dcf77_data!(signal::AbstractVector{T}, port::String, rate::Signed, milliseconds::Signed) where {T<:Integer}
+using Plots: Plots
+
+function read_dcf77_data!(signal::AbstractVector{T}, port::String, rate::Signed, milliseconds::Signed; plot::Bool=false) where {T<:Integer}
     N = length(signal)
     delim = "\r\n"
 
@@ -12,6 +14,9 @@ function read_dcf77_data!(signal::AbstractVector{T}, port::String, rate::Signed,
             data = readuntil(sp, delim)
             s = tryparse(T, data)
             @inbounds signal[idx] = isnothing(s) ? -one(T) : s
+            if plot && isone(mod(idx, round(Int, 1000 / milliseconds)))
+                display(Plots.plot(@view signal[begin:idx]))
+            end
         end
     end
     return signal
@@ -20,8 +25,8 @@ end
 # `millisecond` is the rate at which the data is written to the serial port, `time` is for
 # how long we want to read the data, the length of the data arrays is inferred from these
 # two figures.
-function read_dcf77_data(port::String, rate::Signed, milliseconds::Signed, time::Real)
+function read_dcf77_data(port::String, rate::Signed, milliseconds::Signed, time::Real; plot::Bool=false)
     N = round(Int, time / milliseconds * 1000)
     signal = zeros(Int16, N)
-    return read_dcf77_data!(signal, port, rate, milliseconds)
+    return read_dcf77_data!(signal, port, rate, milliseconds; plot)
 end
